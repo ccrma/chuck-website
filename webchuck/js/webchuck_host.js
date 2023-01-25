1247009f3365ff9dac1b2a7a23e9c97c7159f345
Ge Wang <ge@ccrma.stanford.edu>
2023-01-24 17:03:33 -0800

    updated to webchuck main branch latest

--- webchuck/js/webchuck_host.js
+++ webchuck/js/webchuck_host.js
@@ -6,6 +6,9 @@ var theChuckReady = defer();
 var filesToPreload = [];
 var whereIsChuck = whereIsChuck || "./js";
 var currentChuckID = 1;
+let send
+let visual
+let analyser
 
 var chuckPrint = function( text )
 {
@@ -87,6 +90,7 @@ var startAudioContext = async function()
     audioContext = new AudioContext({
         //sampleRate: 48000
     });
+    await audioContext.resume();
     await audioContext.audioWorklet.addModule( whereIsChuck + '/webchuck.js');
 }
 
@@ -99,9 +103,17 @@ var startChuck = async function()
         var newID = currentChuckID;
         currentChuckID++;
         
+        var cnv = document.getElementById("canvas"),
+        send = new GainNode(audioContext, {gain: 1});
+        analyser = audioContext.createAnalyser();
+        visual = new Visualizer(cnv,analyser)
+        send.connect(analyser);
+        analyser.connect(audioContext.destination);
         theChuck = await createAChuck( newID, theChuckReady );
-        theChuck.connect( audioContext.destination );
+        theChuck.connect(send);
         theChuckAlmostReady.resolve();
+        visual.drawVisualization_()
+        visual.start()
     }
 };
 
